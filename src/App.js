2a01d53e7d94b1867b31644d0ff871cb3d30077f
Mankirat <mankirat95@gmail.com>
2022-03-22 20:24:18 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,6 @@
 import "./App.css";
-
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <p></p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
+    return <div className="App"></div>;
 }
 
 export default App;
