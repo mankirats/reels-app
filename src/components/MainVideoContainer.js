47b729117814cddb12ec394ddcfcce0edfd573aa
Mankirat <mankirat95@gmail.com>
2022-03-24 21:47:33 +0530

    maincomponent changes and styling

--- src/components/MainVideoContainer.js
+++ src/components/MainVideoContainer.js
@@ -1,14 +1,22 @@
 import React from "react";
 import { ScrollingDiv } from "./Style";
 import VideoPlayer from "./VideoPlayer";
-import vid1 from "./vid1.mp4";
-import vid2 from "./vid2.mp4";
+import vid1 from "./VideoFiles/vid1.mp4";
+import vid2 from "./VideoFiles/vid2.mp4";
 
 function MainVideoContainer() {
     return (
         <ScrollingDiv>
-            <VideoPlayer playfile={vid1}></VideoPlayer>
-            <VideoPlayer playfile={vid2}></VideoPlayer>
+            <VideoPlayer
+                playfile={vid1}
+                videoUser="User1"
+                videoDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry"
+            ></VideoPlayer>
+            <VideoPlayer
+                playfile={vid2}
+                videoUser="User2"
+                videoDescription="Description for user 2 videoLorem Ipsum is simply dummy text of the printing and typesetting industry"
+            ></VideoPlayer>
         </ScrollingDiv>
     );
 }
--- src/components/Style.js
+++ src/components/Style.js
@@ -2,17 +2,18 @@ import styled from "styled-components";
 import { devices } from "./devices";
 
 const ScrollingDiv = styled.div`
+    position: relative;
     scroll-snap-type: y mandatory;
     overflow-y: scroll;
     overflow-x: hidden;
     height: 80vh;
-    max-width: calc(20vw + 4rem);
-    scroll-behavior: smooth;
+    width: calc(20vw + 4rem);
+
     @media ${devices.laptop} {
-        width: 45vw;
+        width: calc(25vw + 4rem);
     }
     @media ${devices.mobileL} {
-        width: 100vw;
+        width: calc(100vw - 1rem);
     }
 `;
 
@@ -28,7 +29,51 @@ const StyledVideoPlayer = styled.video`
     object-fit: fill;
     position: relative;
     height: 100%;
-    min-width: 100%;
+    width: 100%;
 `;
 
-export { StyledVideoPlayer, ScrollingDiv };
+const VideoPlayerContainer = styled.div`
+    position: relative;
+    width: 100%;
+    height: 100%;
+`;
+
+const VideoFooterDiv = styled.div`
+    width: calc(100% - 4.5rem);
+    min-height: 5%;
+    position: absolute;
+    bottom: 20px;
+    left: 8px;
+    color: white;
+    font-weight: 100;
+
+    font-size: calc(0.4vh + 0.7rem);
+    letter-spacing: 0.3px;
+    padding: 1rem 1rem;
+`;
+
+const VideoUserDiv = styled.div`
+    font-weight: 200;
+    padding: 0.5rem 0;
+`;
+
+const VideoSidebarDiv = styled.div`
+    position: absolute;
+    bottom: 20px;
+    right: 20px;
+    display: flex;
+    color: white;
+    flex-direction: column;
+    span {
+        padding: 0.5rem 0;
+    }
+`;
+
+export {
+    StyledVideoPlayer,
+    ScrollingDiv,
+    VideoFooterDiv,
+    VideoPlayerContainer,
+    VideoUserDiv,
+    VideoSidebarDiv,
+};
